167e56be2acaddc87ddd25a09c4c8319e43c8197
rodrigoncalves <rodrigosg2000@gmail.com>
2022-04-26 20:26:30 -0300

    Create sale

--- contracts/NFTAuction.sol
+++ contracts/NFTAuction.sol
@@ -15,6 +15,8 @@ contract NFTAuction is ERC721URIStorage, Ownable {
     mapping(uint256 => uint256) private highestBid;
     mapping(uint256 => address) private highestBidder;
 
+    uint256 private ownerEarnings;
+
     struct TokenItem {
         uint256 tokenId;
         address payable seller;
@@ -27,11 +29,27 @@ contract NFTAuction is ERC721URIStorage, Ownable {
 
     constructor() ERC721("NFTAuction", "NFTA") {}
 
+    function getToken(uint256 tokenId) public view returns (TokenItem memory item) {
+        return items[tokenId];
+    }
+
+    function getHighestBidder(uint256 tokenId) public view returns (address) {
+        return highestBidder[tokenId];
+    }
+
+    function getHighestBid(uint256 tokenId) public view returns (uint256) {
+        return highestBid[tokenId];
+    }
+
+    function getOwnerEarnings() public view onlyOwner returns (uint256) {
+        return ownerEarnings;
+    }
+
     function createToken(string memory tokenURI, uint256 price) public payable returns (uint256) {
         _tokenIds.increment();
         uint256 newTokenId = _tokenIds.current();
 
-        _mint(msg.sender, newTokenId);
+        _mint(address(this), newTokenId);
         _setTokenURI(newTokenId, tokenURI);
         createMarketItem(newTokenId, price);
         return newTokenId;
@@ -42,10 +60,6 @@ contract NFTAuction is ERC721URIStorage, Ownable {
         TokenItem memory item = TokenItem(tokenId, payable(msg.sender), payable(address(this)), price, false);
         items[tokenId] = item;
         emit TokenItemCreated(tokenId, msg.sender, msg.sender, price, false);
-    }
-
-    function getToken(uint256 tokenId) public view returns (TokenItem memory item) {
-        return items[tokenId];
     }
 
     // returns all unsold market items
@@ -64,5 +78,35 @@ contract NFTAuction is ERC721URIStorage, Ownable {
             }
         }
         return unsoldItems;
+    }
+
+    function makeABid(uint256 tokenId) public payable {
+        require(highestBid[tokenId] < msg.value, "Bid must be higher than current highest bid");
+        require(items[tokenId].sold == false, "Item has already been sold");
+        require(items[tokenId].price >= msg.value, "Bid must be less than or equal to the price");
+
+        highestBid[tokenId] = msg.value;
+        highestBidder[tokenId] = msg.sender;
+
+        // transfer the token
+        if (items[tokenId].price == msg.value) {
+            createSale(tokenId, msg.value);
+        }
+    }
+
+    function createSale(uint256 tokenId, uint256 bid) private {
+        address seller = items[tokenId].seller;
+        address buyer = address(msg.sender);
+
+        items[tokenId].sold = true;
+        items[tokenId].owner = payable(buyer);
+        items[tokenId].seller = payable(address(0));
+        _itemsSold.increment();
+
+        _transfer(address(this), buyer, tokenId);
+
+        uint256 fee = bid / 10;
+        ownerEarnings += fee;
+        payable(seller).transfer(bid - fee);
     }
 }
--- test/NFTAuction.ts
+++ test/NFTAuction.ts
@@ -6,8 +6,9 @@ import { expect } from 'chai';
 describe.only('NFTAuction', () => {
   let contract: Contract;
   let contractOwner: SignerWithAddress;
-  let user1: SignerWithAddress;
-  let user2: SignerWithAddress;
+  let seller1: SignerWithAddress;
+  let seller2: SignerWithAddress;
+  let buyer1: SignerWithAddress;
 
   beforeEach(async () => {
     // deploy the nft auction
@@ -15,13 +16,17 @@ describe.only('NFTAuction', () => {
     contract = await NFTAuction.deploy();
     await contract.deployed();
 
-    [contractOwner, user1, user2] = await ethers.getSigners();
+    [contractOwner, seller1, seller2, buyer1] = await ethers.getSigners();
 
     // create some tokens
-    let tx = await contract.createToken('https://www.mytokenlocation.com', ethers.utils.parseEther('0.1'));
+    let tx = await contract
+      .connect(seller1)
+      .createToken('https://www.mytokenlocation.com', ethers.utils.parseEther('0.1'));
     await tx.wait();
 
-    tx = await contract.createToken('https://www.mytokenlocation2.com', ethers.utils.parseEther('0.5'));
+    tx = await contract
+      .connect(seller2)
+      .createToken('https://www.mytokenlocation2.com', ethers.utils.parseEther('0.2'));
     await tx.wait();
   });
 
@@ -39,6 +44,62 @@ describe.only('NFTAuction', () => {
     expect(items[0].tokenId).to.eq(1);
     expect(items[0].price).to.eq(ethers.utils.parseEther('0.1'));
     expect(items[1].tokenId).to.eq(2);
-    expect(items[1].price).to.eq(ethers.utils.parseEther('0.5'));
+    expect(items[1].price).to.eq(ethers.utils.parseEther('0.2'));
+  });
+
+  it('should accept a bid from a buyer', async () => {
+    const bid = ethers.utils.parseEther('0.08');
+    await contract.connect(buyer1).makeABid(1, { value: bid });
+
+    const highestBidder = await contract.getHighestBidder(1);
+    expect(highestBidder).to.eq(buyer1.address);
+
+    const highestBid = await contract.getHighestBid(1);
+    expect(highestBid).to.eq(bid);
+  });
+
+  it('should reject a bid from a buyer if the bid is lower than the current highest bid', async () => {
+    const bid = ethers.utils.parseEther('0.08');
+    await contract.connect(buyer1).makeABid(1, { value: bid });
+
+    const bid2 = ethers.utils.parseEther('0.07');
+    await expect(contract.connect(seller2).makeABid(1, { value: bid2 })).to.be.revertedWith(
+      'Bid must be higher than current highest bid'
+    );
+  });
+
+  it('should reject a bid from a buyer if the bid is higher than the token price', async () => {
+    const bid = ethers.utils.parseEther('1');
+    await expect(contract.connect(buyer1).makeABid(1, { value: bid })).to.be.revertedWith(
+      'Bid must be less than or equal to the price'
+    );
+  });
+
+  it.only('should transfer the token if bid met exactly the price', async () => {
+    const initialBalanceSeller1 = await seller1.getBalance();
+
+    // make a bid
+    const bid = ethers.utils.parseEther('0.1');
+    await contract.connect(buyer1).makeABid(1, { value: bid });
+
+    // check that the token is now sold and belongs to the buyer
+    const token = await contract.getToken(1);
+    expect(token.sold).to.be.true;
+    expect(token.owner).to.eq(buyer1.address);
+
+    // check that the item is not available anymore for sale
+    const availableItems = await contract.getAvailableItems();
+    expect(availableItems.length).to.eq(1);
+
+    // check the owner earnings
+    const ownerEarnings = await contract.connect(contractOwner).getOwnerEarnings();
+    expect(ownerEarnings).to.eq(ethers.utils.parseEther('0.01'));
+
+    // check that the seller received the amount minus contract owner fee
+    const finalBalanceSeller1 = await seller1.getBalance();
+    expect(finalBalanceSeller1.sub(initialBalanceSeller1)).to.eq(ethers.utils.parseEther('0.09'));
+
+    // non-owner user cannot check the owner earnings
+    await expect(contract.connect(seller1).getOwnerEarnings()).to.be.revertedWith('Ownable: caller is not the owner');
   });
 });
