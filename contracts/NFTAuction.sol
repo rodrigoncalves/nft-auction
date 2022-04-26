70e800c25c83385f90fc3192b205eee9f0496a65
rodrigoncalves <rodrigosg2000@gmail.com>
2022-04-26 10:52:51 -0300

    List available assets

--- contracts/NFTAuction.sol
+++ contracts/NFTAuction.sol
@@ -12,6 +12,8 @@ contract NFTAuction is ERC721URIStorage, Ownable {
     Counters.Counter private _itemsSold;
 
     mapping(uint256 => TokenItem) private items;
+    mapping(uint256 => uint256) private highestBid;
+    mapping(uint256 => address) private highestBidder;
 
     struct TokenItem {
         uint256 tokenId;
@@ -21,21 +23,11 @@ contract NFTAuction is ERC721URIStorage, Ownable {
         bool sold;
     }
 
-    event TokenItemCreated(
-        uint256 indexed tokenId,
-        address seller,
-        address owner,
-        uint256 price,
-        bool sold
-    );
+    event TokenItemCreated(uint256 indexed tokenId, address seller, address owner, uint256 price, bool sold);
 
     constructor() ERC721("NFTAuction", "NFTA") {}
 
-    function createToken(string memory tokenURI, uint256 price)
-        public
-        payable
-        returns (uint256)
-    {
+    function createToken(string memory tokenURI, uint256 price) public payable returns (uint256) {
         _tokenIds.increment();
         uint256 newTokenId = _tokenIds.current();
 
@@ -47,22 +39,30 @@ contract NFTAuction is ERC721URIStorage, Ownable {
 
     function createMarketItem(uint256 tokenId, uint256 price) private {
         require(price > 0, "Price must be greater than 0");
-        TokenItem memory item = TokenItem(
-            tokenId,
-            payable(msg.sender),
-            payable(address(this)),
-            price,
-            false
-        );
+        TokenItem memory item = TokenItem(tokenId, payable(msg.sender), payable(address(this)), price, false);
         items[tokenId] = item;
         emit TokenItemCreated(tokenId, msg.sender, msg.sender, price, false);
     }
 
-    function getToken(uint256 tokenId)
-        public
-        view
-        returns (TokenItem memory item)
-    {
+    function getToken(uint256 tokenId) public view returns (TokenItem memory item) {
         return items[tokenId];
+    }
+
+    // returns all unsold market items
+    function getAvailableItems() public view returns (TokenItem[] memory) {
+        uint256 itemCount = _tokenIds.current();
+        uint256 unsoldItemCount = _tokenIds.current() - _itemsSold.current();
+        uint256 currentIndex = 0;
+
+        TokenItem[] memory unsoldItems = new TokenItem[](unsoldItemCount);
+        for (uint256 i = 0; i < itemCount; i++) {
+            if (items[i + 1].owner == address(this)) {
+                uint256 currentId = i + 1;
+                TokenItem storage currentItem = items[currentId];
+                unsoldItems[currentIndex] = currentItem;
+                currentIndex++;
+            }
+        }
+        return unsoldItems;
     }
 }
--- test/NFTAuction.ts
+++ test/NFTAuction.ts
@@ -1,9 +1,9 @@
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { Contract } from "ethers";
-import { ethers } from "hardhat";
-import { expect } from "chai";
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { Contract } from 'ethers';
+import { ethers } from 'hardhat';
+import { expect } from 'chai';
 
-describe.only("NFTAuction", () => {
+describe.only('NFTAuction', () => {
   let contract: Contract;
   let contractOwner: SignerWithAddress;
   let user1: SignerWithAddress;
@@ -11,23 +11,34 @@ describe.only("NFTAuction", () => {
 
   beforeEach(async () => {
     // deploy the nft auction
-    const NFTAuction = await ethers.getContractFactory("NFTAuction");
+    const NFTAuction = await ethers.getContractFactory('NFTAuction');
     contract = await NFTAuction.deploy();
     await contract.deployed();
 
     [contractOwner, user1, user2] = await ethers.getSigners();
-  });
 
-  it("should accept an NFT with its price point from the asset owner", async () => {
-    // create a token
-    const tokenURI = "https://www.mytokenlocation.com";
-    const tokenPrice = ethers.utils.parseEther("5");
-    const tx = await contract.createToken(tokenURI, tokenPrice);
+    // create some tokens
+    let tx = await contract.createToken('https://www.mytokenlocation.com', ethers.utils.parseEther('0.1'));
     await tx.wait();
 
+    tx = await contract.createToken('https://www.mytokenlocation2.com', ethers.utils.parseEther('0.5'));
+    await tx.wait();
+  });
+
+  it('should accept an NFT with its price point from the asset owner', async () => {
     // get the token by id
     const token = await contract.getToken(1);
     expect(token.tokenId).to.eq(1);
     expect(token.sold).to.be.false;
+  });
+
+  it('should list the available assets with its price', async () => {
+    const items = await contract.getAvailableItems();
+
+    expect(items.length).to.eq(2);
+    expect(items[0].tokenId).to.eq(1);
+    expect(items[0].price).to.eq(ethers.utils.parseEther('0.1'));
+    expect(items[1].tokenId).to.eq(2);
+    expect(items[1].price).to.eq(ethers.utils.parseEther('0.5'));
   });
 });
